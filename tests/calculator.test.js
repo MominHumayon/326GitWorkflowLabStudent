902124ef9043b4268b01d760c5f10a24291020f5
Adithya <adhitech17@gmail.com>
2024-10-15 23:52:49 -0400

    Implmented Function and 1 test case

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -17,3 +17,10 @@ describe('Calculator Calculation Functions', function() {
 
 
 // Add your tests here
+
+describe('Calculate the Sine values', () => {
+    it('should be correct for 50', () => {
+        const res = calculator.sine(50);
+        assert.strictEqual(res, 0.76604444)
+    });
+});
